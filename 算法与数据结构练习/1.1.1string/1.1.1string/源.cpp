8c28fa47a951e5cc972c582e42030a647bd2ce04
妮旦 <61820149+wqy-nd@users.noreply.github.com>
2020-03-17 13:32:34 +0800

    Add files via upload

---
+++ 算法与数据结构练习/1.1.1string/1.1.1string/源.cpp
@@ -0,0 +1,26 @@
+//字符串倒置
+#include<iostream>
+#include<string>
+#define N 100
+using namespace std;
+void function(char s[N]) {
+	char * head = s, *tail;
+	char temp;
+	int len;
+	len = strlen(s);
+	tail = head + len - 1;
+	//交换后指针向中间移动直至相遇或交错结束
+	while (head <= tail) {
+		temp = *head;
+		*head = *tail;
+		*tail = temp;
+		head++;tail--;
+	}
+	cout << s << endl;
+}
+int main() {
+	char s[N];
+	cin >> s;
+	function(s);
+	return 0;
+}
---
+++ 算法与数据结构练习/1.1.2array/1.1.2array/源.cpp
@@ -0,0 +1,35 @@
+//有效字母异位词
+#include<iostream>
+#include<string>
+using namespace std;
+bool zero(int a[26]) {//判断数组中元素是否均为零，应该有更好的方法吧，暂时没想到
+	for (int i = 0;i < 26;i++) {
+		if (a[i] == 0) continue;
+		else return false;
+	}
+	return true;
+}
+bool judge(string s, string t) {
+	int len1 = s.length(), len2 = t.length(), a[26];
+	for (int i = 0;i < 26;i++)
+		a[i] = 0;
+	if (len1 != len2)
+		return false;
+	else {
+		for (int i = 0;i < len1;i++) {
+			a[s[i] - 97] += 1;
+			a[t[i] - 97] -= 1;
+		}
+		if (zero(a)) return true;
+		else return false;
+	}
+}
+int main() {
+	string s, t;
+	cin >> s;
+	cin >> t;
+	if (judge(s,t))
+		cout << "是异位词" << endl;
+	else
+		cout << "不是异位词" << endl;
+}
